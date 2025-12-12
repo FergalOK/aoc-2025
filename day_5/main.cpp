1f5363ce176a99f33413ae0630a34162d78b0694
fergal <fergal.okane@yahoo.com>
2025-12-12 23:03:27 +0100

    Day 5 part 2

--- day_5/main.cpp
+++ day_5/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <cassert>
 #include <vector>
-#include <unordered_set>
+#include <set>
 #include <regex>
 
 int main(int argc,  char** argv) {
@@ -14,9 +14,9 @@ int main(int argc,  char** argv) {
     std::string line;
     std::regex re("(\\d*)-(\\d*)");
     std::smatch m;
-    std::vector<std::pair<int64_t, int64_t>> ranges;
+    std::set<std::pair<int64_t, int64_t>> ranges;
     while (std::getline(file, line) && std::regex_search(line, m, re)) {
-        ranges.push_back({stol(m[1]), stol(m[2])});
+        ranges.insert({stol(m[1]), stol(m[2])});
     }
     while (std::getline(file, line)) {
         int64_t needle = stol(line);
@@ -29,4 +29,14 @@ int main(int argc,  char** argv) {
     }
 
     std::cout << "Result (part 1): " << result << std::endl;
+
+    int64_t result_part2 = 0;
+    int64_t highest = -1;
+    for (auto [start, end] : ranges) {
+        int64_t new_start = std::max(highest+1, start);
+        result_part2 += std::max(0l, end - new_start + 1);
+        highest = std::max(highest, end);
+    }
+
+    std::cout << "Result (part 2): " << result_part2 << std::endl;
 }
